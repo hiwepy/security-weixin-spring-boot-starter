56cee8f0959fae01af5b08ce03eba3d7f02a4c14
wandl <hiwepy@163.com>
2021-06-02 20:40:44 +0800

    过滤器初始化优化

--- src/main/java/org/springframework/security/boot/SecurityWxMpFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityWxMpFilterConfiguration.java
@@ -15,14 +15,14 @@ import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.boot.biz.authentication.AuthenticationListener;
-import org.springframework.security.boot.biz.authentication.captcha.CaptchaResolver;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
 import org.springframework.security.boot.biz.property.SecuritySessionMgtProperties;
 import org.springframework.security.boot.biz.userdetails.UserDetailsServiceAdapter;
+import org.springframework.security.boot.utils.WebSecurityUtils;
 import org.springframework.security.boot.weixin.authentication.WxMpAuthenticationProcessingFilter;
 import org.springframework.security.boot.weixin.authentication.WxMpAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,9 +33,7 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.savedrequest.RequestCache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -61,13 +59,12 @@ public class SecurityWxMpFilterConfiguration {
     	private final SecurityWxMpAuthcProperties authcProperties;
 
     	private final LocaleContextFilter localeContextFilter;
-    	private final AuthenticationEntryPoint authenticationEntryPoint;
-  	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
-  	    private final AuthenticationFailureHandler authenticationFailureHandler;
-  	    private final ObjectMapper objectMapper;
-      	private final RequestCache requestCache;
-      	private final RememberMeServices rememberMeServices;
-  		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
+	    private final AuthenticationEntryPoint authenticationEntryPoint;
+	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+	    private final AuthenticationFailureHandler authenticationFailureHandler;
+	    private final ObjectMapper objectMapper;
+    	private final RememberMeServices rememberMeServices;
+		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
    		
    		public WxMpWebSecurityConfigurerAdapter(
    			
@@ -76,33 +73,29 @@ public class SecurityWxMpFilterConfiguration {
    				SecurityWxMpAuthcProperties authcProperties,
 
    				ObjectProvider<LocaleContextFilter> localeContextProvider,
-   				ObjectProvider<WxMpAuthenticationProvider> authenticationProvider,
-   				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
+				ObjectProvider<AuthenticationProvider> authenticationProvider,
    				ObjectProvider<AuthenticationListener> authenticationListenerProvider,
    				ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
    				ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
    				ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider,
-   				ObjectProvider<CaptchaResolver> captchaResolverProvider,
-   				ObjectProvider<LogoutHandler> logoutHandlerProvider,
    				ObjectProvider<ObjectMapper> objectMapperProvider,
-   				ObjectProvider<RememberMeServices> rememberMeServicesProvider
+   				ObjectProvider<RememberMeServices> rememberMeServicesProvider,
+   				ObjectProvider<SessionAuthenticationStrategy> sessionAuthenticationStrategyProvider
 
 			) {
    			
-   			super(bizProperties, authcProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()),
-					authenticationManagerProvider.getIfAvailable());
+   			super(bizProperties, sessionMgtProperties, authenticationProvider.stream().collect(Collectors.toList()));
    			
    			this.authcProperties = authcProperties;
 
-			this.localeContextFilter = localeContextProvider.getIfAvailable();
+   			this.localeContextFilter = localeContextProvider.getIfAvailable();
    			List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
-   			this.authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
-   			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
-   			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationEntryPoint = WebSecurityUtils.authenticationEntryPoint(authcProperties, sessionMgtProperties, authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+   			this.authenticationSuccessHandler = WebSecurityUtils.authenticationSuccessHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationFailureHandler = WebSecurityUtils.authenticationFailureHandler(authcProperties, sessionMgtProperties, authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
    			this.objectMapper = objectMapperProvider.getIfAvailable();
-   			this.requestCache = super.requestCache();
    			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
-   			this.sessionAuthenticationStrategy = super.sessionAuthenticationStrategy();
+   			this.sessionAuthenticationStrategy = sessionAuthenticationStrategyProvider.getIfAvailable();
    			
    		}
    		   		
@@ -136,16 +129,12 @@ public class SecurityWxMpFilterConfiguration {
    	    @Override
 		public void configure(HttpSecurity http) throws Exception {
 			
-	    	http.requestCache()
-	        	.requestCache(requestCache)
-	        	.and()
+	    	http.antMatcher(authcProperties.getPathPattern())
 	        	.exceptionHandling()
 	        	.authenticationEntryPoint(authenticationEntryPoint)
 	        	.and()
 	        	.httpBasic()
-	        	.authenticationEntryPoint(authenticationEntryPoint)
-	        	.and()
-	        	.antMatcher(authcProperties.getPathPattern())
+	        	.disable()
 	        	.addFilterBefore(localeContextFilter, UsernamePasswordAuthenticationFilter.class)
 	        	.addFilterBefore(authenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class); 
 
