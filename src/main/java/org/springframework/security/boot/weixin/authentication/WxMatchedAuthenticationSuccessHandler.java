917eb274a52cb0269a7c0d83e3a123609e65ab93
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:48:57 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/springframework/security/boot/weixin/authentication/WxMatchedAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/weixin/authentication/WxMatchedAuthenticationSuccessHandler.java
@@ -24,7 +24,7 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * 微信公共号、小程序认证 (authentication)成功回调器：讲认证信息写回前端
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class WxMatchedAuthenticationSuccessHandler implements MatchedAuthenticationSuccessHandler {
    
