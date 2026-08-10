001bf645f40969d984595b6b8249728ab8c92535
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:05:02 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/weixin/authentication/WxMaLoginRequest.java
+++ src/main/java/org/springframework/security/boot/weixin/authentication/WxMaLoginRequest.java
@@ -24,7 +24,7 @@ import lombok.Data;
 
 /**
  * 微信小程序 Login Request
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
--- src/test/java/org/springframework/security/boot/weixin/authentication/WxMaAuthenticationProcessingFilterTest.java
+++ src/test/java/org/springframework/security/boot/weixin/authentication/WxMaAuthenticationProcessingFilterTest.java
@@ -39,7 +39,7 @@ import static org.mockito.Mockito.when;
 /**
  * Unit tests for {@link WxMaAuthenticationProcessingFilter}.
  *
- * @author <a href="https://github.com/loong10k">[@Loong Wan]</a>
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @DisplayName("WxMaAuthenticationProcessingFilter Tests")
